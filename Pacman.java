964cf6856d67a3aefcaee8fcd9f8332a9bea9240
Julian David Ramirez Lopera <jdramirezl@eafit.edu.co>
2020-11-28 21:33:52 -0500

    changes

--- Pacman.java
+++ Pacman.java
@@ -6,8 +6,9 @@ public class Pacman extends Caracter {
 
     /**
      * Constructor que define los atributos de pacman
-     * @param tipo A traves de este entero diferenciamos si es pacman, arepa, etc
-     * @param posicion Un objeto tipo posicion que nos dice las coordenadas X,Y de pacman
+     *
+     * @param tipo       A traves de este entero diferenciamos si es pacman, arepa, etc
+     * @param posicion   Un objeto tipo posicion que nos dice las coordenadas X,Y de pacman
      * @param puntosVida Es un entero que asigna la cantidad de vida de pacman
      */
     public Pacman(int tipo, Posicion posicion, int puntosVida){
@@ -15,10 +16,20 @@ public class Pacman extends Caracter {
         this.puntosVida = puntosVida;
     }
 
+    /**
+     * Devuelve la vida de pacman
+     *
+     * @return los puntos de vida
+     */
     public int getPuntosVida() {
         return puntosVida;
     }
 
+    /**
+     * Cambia los puntos de vida de pacman
+     *
+     * @param puntosVida Los nuevos puntos de vida
+     */
     public void setPuntosVida(int puntosVida) {
         this.puntosVida = puntosVida;
     }
